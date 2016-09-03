51495331d425bcc64bf08df8bef4aa3b978cc3f8
Sami El Maameri <sami@Samis-MacBook-Air.local>
2016-09-03 11:49:53 +0400

    setup for heroku

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sami/Documents/flask/angular-http/flask"
+set -gx VIRTUAL_ENV "/Users/sami/Documents/flask/beatsblender/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
