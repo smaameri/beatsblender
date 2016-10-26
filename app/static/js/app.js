1bc9126655dcbb70ef8af3e2a9d3e281f886319f
Sami El Maameri <sami@Samis-MacBook-Air.local>
2016-10-26 16:39:25 +0400

    test

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -39,8 +39,8 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $auth
 	  $authProvider.facebook({
 	    clientId: '1162949303786742',
 	    // by default, the redirect URI is http://localhost:5000
-	    //redirectUri: 'http://beatsblender-stage.herokuapp.com'
-	    redirectUri: 'http://localhost:5000/static/partials/login.htm'
+	    redirectUri: 'http://beatsblender-stage.herokuapp.com'
+	    //redirectUri: 'http://localhost:5000/static/partials/login.htm'
 			
 	  });
 	
